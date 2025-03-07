55c94aad07170221858c2ae04fd9ec09cfe64e3e
aloon <118343415+aelaen-1@users.noreply.github.com>
2025-03-07 16:28:20 +0100

    vive les pointeurs sur fonctions

---
+++ ex05/main.cpp
@@ -0,0 +1,12 @@
+#include "Harl.hpp"
+
+int main ()
+{
+    Harl    harler;
+
+    harler.complain("DEBUG");
+    harler.complain("WARNING");
+    harler.complain("INFO");
+    harler.complain("ERROR");
+    return (0);
+}
